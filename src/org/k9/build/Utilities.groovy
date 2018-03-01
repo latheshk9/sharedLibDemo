927302ad59cb001b2f14519b43d815781b878bb3
latheshk9 <35732885+latheshk9@users.noreply.github.com>
2018-03-01 11:34:15 +0530

    Update Utilities.groovy

--- src/org/k9/build/Utilities.groovy
+++ src/org/k9/build/Utilities.groovy
@@ -1,9 +1,6 @@
 package org.k9.build
-class Utilities implements Serializable {
-  def info(message) {
-    echo "INFO: ${message}"
-  }
-    def warning(message){
-        echo "WARNING: ${message}"
-    }
+def call(String name = 'human') {
+    // Any valid steps can be called from this code, just like in other
+    // Scripted Pipeline
+    echo "Hello, ${name}."
 }
