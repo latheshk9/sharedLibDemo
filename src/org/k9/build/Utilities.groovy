60e75a7820cb7dc39e39635c4847a335c298c4a1
latheshk9 <35732885+latheshk9@users.noreply.github.com>
2018-03-01 11:45:24 +0530

    Update Utilities.groovy

--- src/org/k9/build/Utilities.groovy
+++ src/org/k9/build/Utilities.groovy
@@ -1,8 +1,6 @@
 package org.k9.build 
-class Utilities implements Serializable {
-  def steps
-  Utilities(steps) {this.steps = steps}
-  def mvn(args) {
-    steps.sh "${steps.tool 'Maven'}/bin/mvn -o ${args}"
-  }
+def checkOutFrom(repo) {
+  git url: "git@github.com:jenkinsci/${repo}"
 }
+
+return this
